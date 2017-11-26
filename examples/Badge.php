2d8aa84824169e43072954146049d223020291fd
Dean Blackborough <dean@g3d-development.com>
2017-11-26 00:22:02 +0000

    Html
    * Added generated HTML to example scripts

--- examples/Badge.php
+++ examples/Badge.php
@@ -6,12 +6,18 @@ require __DIR__ . '/../src/Badge.php';
 
 # Minimal example
 $badge = ViewHelper::badge('Badge label');
-echo $badge;
+echo $badge . PHP_EOL;
+
+// Outputs: <span class="badge">Badge label</span>
 
 # Display as a link
 $badge = ViewHelper::badge('Badge label as link');
-echo $badge->asLink('http://www.google.com');
+echo $badge->asLink('http://www.google.com')  . PHP_EOL;
+
+// Outputs: <a href="http://www.google.com" class="badge">Badge label as link</a>
 
 # Display as pill with danger bg style
 $badge = ViewHelper::badge('Badge label');
-echo $badge->pill()->setBgStyle('danger');
+echo $badge->pill()->setBgStyle('danger') . PHP_EOL;
+
+// Outputs: <span class="badge badge-danger badge-pill">Badge label</span>
--- examples/Jumbotron.php
+++ examples/Jumbotron.php
@@ -6,12 +6,18 @@ require __DIR__ . '/../src/Jumbotron.php';
 
 # Minimal example
 $jumbotron = ViewHelper::jumbotron('Heading', '<p>Content</p>');
-echo $jumbotron;
+echo $jumbotron . PHP_EOL;
+
+// Outputs: <div class="jumbotron"><h1 class="display-1">Heading</h1><p>Content</p></div>
 
 # Display with fluid class and primary background class
 $jumbotron = ViewHelper::jumbotron('Heading', '<p>Content</p>');
-echo $jumbotron->fluid()->setBgStyle('primary');
+echo $jumbotron->fluid()->setBgStyle('primary') . PHP_EOL;
+
+// Outputs: <div class="jumbotron jumbotron-fluid bg-primary"><div class="container"><h1 class="display-1">Heading</h1><p>Content</p></div></div>
 
 # Display with heading level 3
 $jumbotron = ViewHelper::jumbotron('Heading', '<p>Content</p>');
-echo $jumbotron->setHeadingDisplayLevel(3);
+echo $jumbotron->setHeadingDisplayLevel(3) . PHP_EOL;
+
+// Outputs: <div class="jumbotron"><h1 class="display-3">Heading</h1><p>Content</p></div>
